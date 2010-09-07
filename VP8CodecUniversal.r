7e40f18fd484ca95349d2992307043005fdd64f7
Ryan Thompson <ryanthompson@google.com>
2010-09-07 12:35:21 -0400

    Removed macros Decompressor/Compressor builds were separated by macros in the original sample code, this no longer is functional.
    Change-Id: I6b16daf1c9cb75d68def31a78b040ff8bed40cec

--- VP8CodecUniversal.r
+++ VP8CodecUniversal.r
@@ -7,69 +7,6 @@
 // be found in the AUTHORS file in the root of the source tree.
 
 
-/*
- 
- © Copyright 2005 Apple Computer, Inc. All rights reserved.
- 
- IMPORTANT:  This Apple software is supplied to 
- you by Apple Computer, Inc. ("Apple") in 
- consideration of your agreement to the following 
- terms, and your use, installation, modification 
- or redistribution of this Apple software 
- constitutes acceptance of these terms.  If you do 
- not agree with these terms, please do not use, 
- install, modify or redistribute this Apple 
- software.
- 
- In consideration of your agreement to abide by 
- the following terms, and subject to these terms, 
- Apple grants you a personal, non-exclusive 
- license, under Apple's copyrights in this 
- original Apple software (the "Apple Software"), 
- to use, reproduce, modify and redistribute the 
- Apple Software, with or without modifications, in 
- source and/or binary forms; provided that if you 
- redistribute the Apple Software in its entirety 
- and without modifications, you must retain this 
- notice and the following text and disclaimers in 
- all such redistributions of the Apple Software. 
- Neither the name, trademarks, service marks or 
- logos of Apple Computer, Inc. may be used to 
- endorse or promote products derived from the 
- Apple Software without specific prior written 
- permission from Apple.  Except as expressly 
- stated in this notice, no other rights or 
- licenses, express or implied, are granted by 
- Apple herein, including but not limited to any 
- patent rights that may be infringed by your 
- derivative works or by other works in which the 
- Apple Software may be incorporated.
- 
- The Apple Software is provided by Apple on an "AS 
- IS" basis.  APPLE MAKES NO WARRANTIES, EXPRESS OR 
- IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED 
- WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY 
- AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING 
- THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE 
- OR IN COMBINATION WITH YOUR PRODUCTS.
- 
- IN NO EVENT SHALL APPLE BE LIABLE FOR ANY 
- SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
- DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
- OF USE, DATA, OR PROFITS; OR BUSINESS 
- INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
- REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF 
- THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER 
- UNDER THEORY OF CONTRACT, TORT (INCLUDING 
- NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN 
- IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF 
- SUCH DAMAGE.
- 
- */
-
-
-
 
 // Set to 1 == building Mac OS X
 #define TARGET_REZ_CARBON_MACHO 1
@@ -97,9 +34,6 @@
 #error Must be building on Windows
     #define TARGET_REZ_WIN32 1
 #endif
-
-#define DECO_BUILD 1
-#define COMP_BUILD 1
 
 #define thng_RezTemplateVersion 2
 
@@ -153,7 +87,6 @@ resource 'STR ' (256) {
 	"VP8"
 };
 
-#if DECO_BUILD
 resource 'thng' (256) {
 	decompressorComponentType,		// Type			
 	kVP8CodecFormatType,				// SubType
@@ -212,9 +145,7 @@ resource 'STR ' (257) {
 resource 'dlle' (256) {
 	"VP8_Decoder_ComponentDispatch"
 };
-#endif // DECO_BUILD
 
-#if COMP_BUILD
 resource 'thng' (258) {
 	compressorComponentType,		// Type			
 	kVP8CodecFormatType,				// SubType
@@ -272,7 +203,6 @@ resource 'STR ' (258) {
 resource 'dlle' (258) {
 	"VP8_Encoder_ComponentDispatch"
 };
-#endif // COMP_BUILD
 
 
 #define kExporterFlags canMovieExportFiles | canMovieExportValidateMovie | \
