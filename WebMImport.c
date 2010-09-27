d71a224ae9293142822c1225808900bb4dd2a0f0
Jeff Koppi <jeffkoppi@google.com>
2010-09-27 17:45:51 -0400

    CreateVP8ImageDescription(), begin QT stuff, reorder files in proj
    Change-Id: I2f01bcf49b9253dbc6e0f56c2c8c7aca20201101

--- WebMImport.c
+++ WebMImport.c
@@ -9,7 +9,7 @@
 
 #include "WebMImport.h"
 
-#include <Carbon/Carbon.h>
+#include <Carbon/Carbon.h> 
 #include <QuickTime/QuickTime.h>
 
 //#include "mkvreader_wrap.h"
@@ -49,6 +49,8 @@ extern "C" {
   pascal ComponentResult WebMImportOpen(WebMImportGlobals store, ComponentInstance self);
   //...
 }
+
+OSErr CreateVP8ImageDescription(long width, long height, ImageDescriptionHandle *descOut);
 
 //--------------------------------------------------------------------------------
 static const wchar_t* utf8towcs(const char* str)
@@ -152,7 +154,8 @@ pascal ComponentResult WebMImportDataRef(WebMImportGlobals store, Handle dataRef
                                          long inFlags, long *outFlags)
 {
   OSErr err = noErr;
-  Track videoTrack = NULL;
+  ::Track movieVideoTrack = NULL;
+  ImageDescriptionHandle vp8DescHand = NULL;
   ComponentInstance dataHandler = 0;
   
   dbg_printf("[WebM Import]  >> [%08lx] :: FromDataRef(dataRef = %08lx, atTime = %ld)\n", (UInt32) store, (UInt32) dataRef, atTime); // targetTrack != NULL
@@ -291,6 +294,7 @@ pascal ComponentResult WebMImportDataRef(WebMImportGlobals store, Handle dataRef
     const long long timeCode = webmCluster->GetTimeCode();
     const long long time_ns = webmCluster->GetTime();
     const BlockEntry* webmBlockEntry = webmCluster->GetFirst();
+    
     //
     //  WebM Block
     //
@@ -303,60 +307,63 @@ pascal ComponentResult WebMImportDataRef(WebMImportGlobals store, Handle dataRef
       const long size = webmBlock->GetSize();
       const long long time_ns = webmBlock->GetTime(webmCluster);
 
-      dbg_printf("\t\t\tBlock\t\t:%s,%15ld,%s,%15lld\n",
-             (trackType == VIDEO_TRACK) ? "V" : "A",
-             size,
-             webmBlock->IsKey() ? "I" : "P",
-             time_ns);
+      dbg_printf("\t\t\tBlock\t\t:%s,%15ld,%s,%15lld\n", (trackType == VIDEO_TRACK) ? "V" : "A", size, webmBlock->IsKey() ? "I" : "P", time_ns);
       
       if (trackType == VIDEO_TRACK) {
         //
-        // Data
+        // WebM Video Data
         //
-
+        const mkvparser::VideoTrack* const webmVideoTrack = static_cast<const VideoTrack* const>(webmTrack);
+        long long width = webmVideoTrack->GetWidth();
+        long long height = webmVideoTrack->GetHeight();
+        dbg_printf("width = %ld\n", width);
+        dbg_printf("height = %ld\n", height);
+        
         // read frame data from WebM Block into buffer
-        unsigned char* buf = (unsigned char*)malloc(size);
+        unsigned char* buf = (unsigned char*)malloc(size);  // vp8 frame
         status = webmBlock->Read(&reader, buf);
 
-        // ****
+        // QuickTime movie stuff begins here...
+        // while {
+        //    NewMovieTrack()
+        //    NewTrackMedia()
+        //    SetTrackEnabled()
+        //    AddMediaSampleReference()
+        //    incr to next frame
+        // InsertMediaIntoTrack()
+        // GetTrackDuration()
+#if 0        
+        Media movieVideoMedia;  // declare here, or at top of function?
+        CreateVP8ImageDescription(width, height, &vp8DescHand);
+        if (movieVideoTrack == NULL) {  // the quicktime movie track (not WebM file track)
+          movieVideoTrack = NewMovieTrack(theMovie, (**vp8DescHand).width << 16, (**vp8DescHand).height << 16, kNoVolume);
+        } // end quicktime movie track
+#endif
         
-      }
+      } // end video track
       
       webmBlockEntry = webmCluster->GetNext(webmBlockEntry);
-    }
+    } // end block loop
     
     webmCluster = webmSegment->GetNext(webmCluster);
-  }
-  
-  
-  // while {
-  //    NewMovieTrack()
-  //    NewTrackMedia()
-  //    SetTrackEnabled()
-  //    AddMediaSampleReference()
-  //    incr to next frame
-  // InsertMediaIntoTrack()
-  // GetTrackDuration()
-  
+  } // end cluster loop
+ 
   
   ImageDescriptionHandle videoDesc = NULL;
   Ptr colors = NULL;
   
-  
-  // ****
-  
 bail:
-	if (videoTrack) { // QT videoTrack
+	if (movieVideoTrack) { // QT videoTrack
 		if (err) {
-			DisposeMovieTrack(videoTrack);
-			videoTrack = NULL;
+			DisposeMovieTrack(movieVideoTrack);
+			movieVideoTrack = NULL;
 		} else {
 			// Set the outFlags to reflect what was done.
 			*outFlags |= movieImportCreateTrack;
 		}
 	}
-	if (videoDesc)
-		DisposeHandle((Handle)videoDesc);
+	if (vp8DescHand)
+		DisposeHandle((Handle)vp8DescHand);
   
 	if (colors)
 		DisposePtr(colors);
@@ -368,7 +375,7 @@ bail:
 	// Return the track identifier of the track that received the imported data in the usedTrack pointer. Your component
 	// needs to set this parameter only if you operate on a single track or if you create a new track. If you modify more
 	// than one track, leave the field referred to by this parameter unchanged. 
-	if (usedTrack) *usedTrack = videoTrack;
+	if (usedTrack) *usedTrack = movieVideoTrack;
 
   dbg_printf("[WebM Import]  << [%08lx] :: FromDataRef(%d, %ld)\n", (UInt32) store, targetTrack != NULL, atTime);
 
@@ -389,3 +396,41 @@ pascal ComponentResult WebMImportGetMIMETypeList(WebMImportGlobals store, QTAtom
 // MovieImportValidate
 // MovieImportValidateDataRef
 // MovieImportRegister
+
+
+//--------------------------------------------------------------------------------
+// CreateImageDescriptionVP8
+// Create image description for a VP8 video frame so that QT can find codec component to decode it.
+// see ImageCompression.h
+OSErr CreateVP8ImageDescription(long width, long height, ImageDescriptionHandle *descOut)
+{
+  OSErr err = noErr;
+  ImageDescriptionHandle descHand = NULL;
+  ImageDescriptionPtr descPtr;
+
+  descHand = (ImageDescriptionHandle)NewHandleClear(sizeof(ImageDescription));
+  if (err = MemError()) goto bail;
+  
+  descPtr = *descHand;
+  descPtr->idSize = sizeof(ImageDescription);     // total size of this image description structure
+  descPtr->cType = kVP8CodecFormatType;           // type of compressor component that created this compressed data
+  descPtr->vendor = kGoogManufacturer;            // developer of compressor that created compressed image
+  descPtr->frameCount = 1;                        // number of frames in the image data
+  descPtr->depth = codecInfoDepth24;              // pixel depth specified for the compressed image
+  descPtr->clutID = -1;                           // id of color table for compressed image, -1 if color table not used
+  descPtr->hRes = 72L << 16;                      // horizontal resolution dpi ****
+  descPtr->vRes = 72L << 16;                      // vertical resolution dpi
+  descPtr->width = width;                         // image width in pixels (passed into this routine)
+  descPtr->height = height;                       // image height in pixels
+  descPtr->version = 0;                           // version of the compressed data
+  
+bail:
+  if (descHand && err) {
+    DisposeHandle((Handle)descHand);
+    descHand = NULL;
+  }
+  
+  *descOut = descHand;
+
+  return err;
+}
