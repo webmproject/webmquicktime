26c1164c531b3729b9da96f18e648f3a77157106
Tom Finegan <tomfinegan@google.com>
2012-02-08 17:05:12 -0500

    Rewrite installer, add XiphQT and updater.
    Add separate installers for the XiphQT and WebM components, and a
    third installer that installs the GoogleSoftwareUpdate, XiphQT, WebM
    packages.
    
    Add preinstall scripts for each QuickTime component. Both remove
    existing versions of the component being installed. Note that the
    preinstall scripts are also run at preupgrade time.
    
    Add GoogleSoftwareUpdate.pkg and contents to provide automatic
    updates after WebM QuickTime package installation.
    
    Add postinstall scripts to register each component with the updater.
    Note that the postinstall scripts are also run at postupgrade time.
    
    Add a build script for creation of the XiphQT and WebM component
    installers. The final installer must be built manually-- this is
    because PackageMaker *always* crashes when attempting to build the
    package on the command line.
    
    Delete the original installer.
    
    Change-Id: Iadfd80befb61d4431164dbc1da9b5ac49f51355c

---
+++ installer/scripts/xiphqt/postinstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+##
+##  Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+##
+##  Use of this source code is governed by a BSD-style license
+##  that can be found in the LICENSE file in the root of the source
+##  tree. An additional intellectual property rights grant can be found
+##  in the file PATENTS.  All contributing project authors may
+##  be found in the AUTHORS file in the root of the source tree.
+readonly KEYSTONE="/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/MacOS/ksadmin"
+readonly QT_SYS_PATH="/Library/QuickTime/"
+readonly XIPHQT_COMPONENT="XiphQT.component"
+readonly XIPHQT_ID="org.xiph.xiph-qt.xiphqt"
+readonly XIPHQT_VERSION="0.1.9"
+readonly UPDATE_URL="TODO"
+
+dbglog() {
+  logger "XiphQT postinstall: $@"
+}
+
+dbglog "script args=$@"
+
+${KEYSTONE} --register \
+  --productid "${XIPHQT_ID}" \
+  --version "${XIPHQT_VERSION}" \
+  --xcpath "${QT_SYS_PATH}{XIPHQT_COMPONENT}" \
+  --url "${UPDATE_URL}"
