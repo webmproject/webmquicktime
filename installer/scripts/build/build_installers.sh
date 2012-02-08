d8815259b15cf476c4a2112275117572a5b33188
Tom Finegan <tomfinegan@google.com>
2012-02-08 17:05:12 -0500

    Rewrite installer, add XiphQT and updater.
    Add separate installers for the XiphQT and WebM components, and a
    third installer that installs the GoogleSoftwareUpdate, XiphQT, WebM
    packages.
    
    Add preinstall scripts for each QuickTime component. Both remove
    existing versions of the component being installed. Note that the
    preinstall scripts are also run at preupgrade time.
    
    Add GoogleSoftwareUpdate.pkg and contents to provide automatic
    updates after WebM QuickTime package installation.
    
    Add postinstall scripts to register each component with the updater.
    Note that the postinstall scripts are also run at postupgrade time.
    
    Add a build script for creation of the XiphQT and WebM component
    installers. The final installer must be built manually-- this is
    because PackageMaker *always* crashes when attempting to build the
    package on the command line.
    
    Delete the original installer.
    
    Change-Id: Iadfd80befb61d4431164dbc1da9b5ac49f51355c

---
+++ installer/scripts/build/build_installers.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+##
+##  Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+##
+##  Use of this source code is governed by a BSD-style license
+##  that can be found in the LICENSE file in the root of the source
+##  tree. An additional intellectual property rights grant can be found
+##  in the file PATENTS.  All contributing project authors may
+##  be found in the AUTHORS file in the root of the source tree.
+
+# Builds the WebM QuickTime and XiphQT component installers, and then
+# includes each package in one final mpkg along with
+# GoogleSoftwareUpdate.pkg
+
+dbglog() {
+  echo "build_installers: $@"
+}
+
+#readonly PKGMAKER="/Developer/usr/bin/packagemaker"
+readonly PKGMAKER="/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker"
+if [[ ! -e "${PKGMAKER}" ]]; then
+  dbglog "ERROR, ${PKGMAKER} does not exist."
+  exit 1
+fi
+
+build_installer() {
+  local readonly PMDOC="$1"
+  local readonly PACKAGE="$2"
+  local readonly RM="rm -r -f"
+
+  # Confirm the pmdoc exists.
+  if [[ ! -e "${PMDOC}" ]]; then
+    dbglog "ERROR, PMDOC ${PMDOC} does not exist!"
+    exit 1
+  fi
+
+  # Delete the output package if it exists.
+  if [[ -e "${PACKAGE}" ]]; then
+    ${RM} "${PACKAGE}"
+  fi
+
+  # Build the package.
+  ${PKGMAKER} -d "${PMDOC}" -o "${PACKAGE}"
+
+  # Confirm that the package was built.
+  if [[ ! -e "${PACKAGE}" ]]; then
+    dbglog "ERROR, build failed for PACKAGE ${PACKAGE}!"
+    exit 1
+  fi
+
+  dbglog "${PACKAGE} build successful."
+}
+
+# WebM components installer project and output package file names.
+readonly WEBM_PMDOC="webm_component_install.pmdoc"
+readonly WEBM_PACKAGE="webm_component_installer.pkg"
+
+# XiphQT components installer project and output package file names.
+readonly XIPHQT_PMDOC="xiphqt_component_install.pmdoc"
+readonly XIPHQT_PACKAGE="xiphqt_component_installer.pkg"
+
+# Full installer project and output package file names.
+readonly INSTALLER_PMDOC="installer.pmdoc"
+readonly INSTALLER_PACKAGE="WebM QuickTime Installer.mpkg"
+
+build_installer "${WEBM_PMDOC}" "${WEBM_PACKAGE}"
+build_installer "${XIPHQT_PMDOC}" "${XIPHQT_PACKAGE}"
+
+# Nice long note about PackageMaker manual build requirements. Why, oh why,
+# does the stupid thing have to crash _every_ time when used on the command
+# line.
+dbglog "${INSTALLER_PMDOC} must be opened in PackageMaker and built manually"
+dbglog "to create ${INSTALLER_PACKAGE}. PackageMaker will always crash when"
+dbglog "${INSTALLER_PMDOC} is built from the command line."
+
+dbglog "Done."
---
+++ installer/scripts/webm/postinstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##
+##  Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+##
+##  Use of this source code is governed by a BSD-style license
+##  that can be found in the LICENSE file in the root of the source
+##  tree. An additional intellectual property rights grant can be found
+##  in the file PATENTS.  All contributing project authors may
+##  be found in the AUTHORS file in the root of the source tree.
+readonly KEYSTONE="/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/MacOS/ksadmin"
+readonly QT_SYS_PATH="/Library/QuickTime/"
+readonly WEBM_COMPONENT="AWebM.component"
+readonly WEBM_ID="org.webmproject.webmquicktime.component"
+readonly WEBM_VERSION="0.3.0"
+readonly UPDATE_URL="TODO"
+
+dbglog() {
+  logger "WebM postinstall: $@"
+}
+
+dbglog "script args=$@"
+
+${KEYSTONE} --register --productid "${WEBM_ID}" --version "${VERSION}" \
+  --xcpath "${QT_SYS_PATH}{WEBM_COMPONENT}" --url "${UPDATE_URL}"
