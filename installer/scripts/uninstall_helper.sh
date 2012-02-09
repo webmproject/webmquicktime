9f3821bfd6132882567f9f28fa909b502cf4ab19
Tom Finegan <tomfinegan@google.com>
2012-02-09 18:41:19 -0500

    Add uninstaller.
    Simple apple script invokes a shell script as administrator to
    delete the components, and then unregister them using ksadmin.
    Intended for inclusion in the disk image side-by-side with the
    installation package.
    
    Change-Id: I946bf3e32e77a8a62c32b433421cfd20f2a5d78c

---
+++ installer/scripts/uninstall_helper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+##
+##  Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+##
+##  Use of this source code is governed by a BSD-style license
+##  that can be found in the LICENSE file in the root of the source
+##  tree. An additional intellectual property rights grant can be found
+##  in the file PATENTS.  All contributing project authors may
+##  be found in the AUTHORS file in the root of the source tree.
+KEYSTONE="/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/MacOS/ksadmin"
+QT_SYS_PATH="/Library/QuickTime/"
+RM="rm -r -f"
+WEBM_COMPONENT="AWebM.component"
+WEBM_ID="org.webmproject.webmquicktime.component"
+XIPHQT_COMPONENT="XiphQT.component"
+XIPHQT_ID="org.xiph.xiph-qt.xiphqt"
+
+dbglog() {
+  logger "WebM QuickTime uninstall_helper: $@"
+}
+
+if [[ -e "${KEYSTONE}" ]]; then
+  # Unregister the component bundles.
+  ${KEYSTONE} --delete --productid ${XIPHQT_ID}
+  ${KEYSTONE} --delete --productid ${WEBM_ID}
+fi
+
+# Delete the WebM component bundle.
+if [[ -e "${QT_SYS_PATH}${WEBM_COMPONENT}" ]]; then
+  dbglog "${RM} ${QT_SYS_PATH}${WEBM_COMPONENT}"
+  ${RM} "${QT_SYS_PATH}${WEBM_COMPONENT}"
+else
+  dbglog "${QT_SYS_PATH}${WEBM_COMPONENT} does not exist."
+fi
+
+# Delete the XiphQT component bundle.
+if [[ -e "${QT_SYS_PATH}${XIPHQT_COMPONENT}" ]]; then
+  dbglog "${RM} ${QT_SYS_PATH}${XIPHQT_COMPONENT}"
+  ${RM} "${QT_SYS_PATH}${XIPHQT_COMPONENT}"
+else
+  dbglog "${QT_SYS_PATH}${XIPHQT_COMPONENT} does not exist."
+fi
+
+dbglog "Done."
