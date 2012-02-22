954c994b0dca3e10f07fea48e34cfaa74a5bf615
Tom Finegan <tomfinegan@google.com>
2012-02-22 18:09:34 -0500

    Add utility script/stop duping tiny bash funcs.
    Well, add the utility script anyway. Removing duped stuff comes
    in subsequent commits.
    
    Change-Id: Icf16a0e61402d660a72136c3777bf9047a39a97b

---
+++ installer/scripts/build/util.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+##
+##  Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+##
+##  Use of this source code is governed by a BSD-style license
+##  that can be found in the LICENSE file in the root of the source
+##  tree. An additional intellectual property rights grant can be found
+##  in the file PATENTS.  All contributing project authors may
+##  be found in the AUTHORS file in the root of the source tree.
+
+# Outputs arguments in bold red text using echo.
+errorlog() {
+  echo -e "$(basename "$0"): \x1b[1;31;01m$@\x1b[0m"
+}
+
+# Outputs arguments in bold yellow text using echo.
+warninglog() {
+  echo -e "$(basename "$0"): \x1b[1;33;01m$@\x1b[0m"
+}
+
+# Outputs arguments in bold green text using echo.
+debuglog() {
+  echo -e "$(basename "$0"): \x1b[1;32;01m$@\x1b[0m"
+}
+
+# Outputs arguments in bold text using echo.
+infolog() {
+  echo -e "$(basename "$0"): \x1b[1m$@\x1b[0m"
+}
+
+# Outputs arguments in blinking bold red text exit's with failure code.
+die() {
+  echo -e "$(basename "$0"): \x1b[5;31;01mERROR: $@\x1b[0m"
+  exit 1
+}
+
+# Copies a bundle while preserving permissions.
+copy_bundle() {
+  local readonly BUNDLE="$1"
+  local readonly TARGET="$2"
+
+  if [[ -z "${BUNDLE}" ]] || [[ ! -e "${BUNDLE}" ]]; then
+    die "ERROR, invalid bundle path passed to copy_bundle, quitting." \
+        "bundle path=${BUNDLE}"
+  fi
+
+  if [[ -z "${TARGET}" ]]; then
+    die "ERROR, invalid target path passed to copy_bundle, quitting." \
+        "target path=${TARGET}"
+  fi
+
+  local readonly COPY="cp -p -R"
+  ${COPY} "${BUNDLE}" "${TARGET}"
+}
+
+file_exists() {
+  test -n "$1" && test -e "$1"
+}
+
