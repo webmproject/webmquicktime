7739baea2f1da83cd4571ab17a31d802433f407e
Tom Finegan <tomfinegan@google.com>
2012-02-22 15:58:39 -0500

    Add script to read version/ID from bundle plist.
    Change-Id: Idc133de36ae2369331c190f0197fb820bf6b0d47

---
+++ installer/scripts/build/read_bundle_plist.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+##
+##  Copyright (c) 2012 The WebM project authors. All Rights Reserved.
+##
+##  Use of this source code is governed by a BSD-style license
+##  that can be found in the LICENSE file in the root of the source
+##  tree. An additional intellectual property rights grant can be found
+##  in the file PATENTS.  All contributing project authors may
+##  be found in the AUTHORS file in the root of the source tree.
+set -e
+
+readonly BUNDLE="$1"
+readonly PLIST_BUDDY="/usr/local/bin/PlistBuddy"
+
+dbglog() {
+  echo "$(basename "$0"): $@"
+}
+
+die() {
+  dbglog "ERROR, $@"
+  exit 1
+}
+
+file_exists() {
+  return $(test -e "$1")
+}
+
+read_bundle_info() {
+  local readonly NAME="$1"
+  local readonly COMMAND="Print :CFBundle${NAME}"
+  local readonly PLIST_PATH="${BUNDLE}/Contents/Info.plist"
+  file_exists "${PLIST_PATH}" || die "${PLIST_PATH} does not exist."
+  echo $(${PLIST_BUDDY} -c "${COMMAND}" "${PLIST_PATH}")
+}
+
+read_bundle_id() {
+  echo $(read_bundle_info Identifier)
+}
+
+read_bundle_version() {
+  echo $(read_bundle_info Version)
+}
+
+file_exists "${PLIST_BUDDY}" || die "${PLIST_BUDDY} does not exist."
+file_exists "${BUNDLE}" || die "${BUNDLE} does not exist."
+
+shopt -s nocasematch
+if [[ "$2" =~ ^v$|ver|version ]]; then
+  read_bundle_version
+elif [[ "$2" =~ ^id$|identifier ]]; then
+  read_bundle_id
+elif [[ -z "$2" ]]; then
+  echo $(read_bundle_id) $(read_bundle_version)
+fi
+shopt -u nocasematch
